1ec135eb7bbccd948c8d1026140ea914ee172217
John Jolly <jjolly@jjolly.dev>
2025-07-13 05:48:50 -0600

    Added initial window

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+use eframe::egui;
+
+struct TriangleApp;
+
+impl eframe::App for TriangleApp {
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        egui::CentralPanel::default().show(ctx, |ui| {
+            ui.heading("Sherman's Line Visualization");
+        });
+    }
+}
+fn main() -> eframe::Result<()> {
+    let options = eframe::NativeOptions::default();
+    eframe::run_native(
+        "Sherman's Line Visualization",
+        options,
+        Box::new(|_cc| Box::new(TriangleApp)),
+    )
 }
